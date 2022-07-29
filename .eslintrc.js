e4c0c11256bd19b0aebcb74bbc8b52fd5c49d74d
flesage-000 <flesage.spam@gmail.com>
2022-07-29 14:57:43 +0200

    Change ESLint options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
   "env": {
-      "browser": true,
-      "es2021": true,
-      "node":true
+    "browser": true,
+    "es2021": true,
+    "node":true,
+    "module":true
   },
-  "extends": "eslint:recommended",
   "parserOptions": {
-      "ecmaVersion": "latest",
-      "no-case-declarations": "error"
+    "ecmaVersion": "latest"
   },
   "rules": {
-      "no-console": "off",
-      "no-invalid-regexp": "warn",
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
-      "no-undef":"off"
+    "no-console": "off",
+    "no-invalid-regexp": "warn",
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "no-undef":"off"
   }
 };
